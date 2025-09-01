fd26e247661127a30d4a23d2f51b5bb856f65019
Amrita Matta <matta.amrita@gmail.com>
2025-09-01 10:26:59 +0300

    feat:"Python POST request"

--- APIRequests/GetRequest.py
+++ APIRequests/GetRequest.py
@@ -8,14 +8,29 @@ import requests
 baseurl = "https://jsonplaceholder.typicode.com"
 
 
-# get request
+# Get request
 def get_request():
     url = baseurl + "/todos/1"
     headers = {"Accept": "application/json", "Content-Type": "application/json"}
     response = requests.get(url, headers=headers)
     assert response.status_code == 200
-    print(response.json())
+    json_data = response.json()
+    json_str = json.dumps(json_data, indent=4)
+    print(json_str)
+    return response.json()
+
+
+# Post request
+def post_request():
+    url = baseurl + "/posts"
+    data = {"userId": 1, "title": "POST | PUT | DELETE", "completed": False}
+    response = requests.post(url, json=data)
+    assert response.status_code == 201
+    json_data = response.json()
+    json_str = json.dumps(json_data, indent=4)
+    print(json_str)
     return response.json()
 
 
 get_request()
+post_request()
